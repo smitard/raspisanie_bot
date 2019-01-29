fd168125a21cf79c074f490d8fb15c3360d3082c
smitard <45298247+smitard@users.noreply.github.com>
2019-01-30 00:36:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot
 import schedule
-import time
 import datetime
 
 rozklad = [{
@@ -44,9 +43,6 @@ rozklad = [{
 
 bot = telebot.TeleBot('662931082:AAEwU55ZVpylddfg75lAnSIs1CQ-SKWfZjA')
 
-
-
-
 # Счет недели. Если она чётная, то это чиситель(True), а если нечётная, то знаменатель(False)
 def numerator():
     if int(datetime.date.today().isoweekday()) < 5:
@@ -70,9 +66,9 @@ def main_message():
         main_message = a[0][0] + '  ' + tomorrowdate.strftime("%d.%m.%Y") +'\n\nЧислитель\n\n1. ' + a[0][1] + '\n2. ' + a[0][2] + '\n3. ' + a[0][3][0] + '\n4. ' + a[0][4] 
     elif numerator() == 0:
         main_message =  a[0][0] + '  ' + tomorrowdate.strftime("%d.%m.%Y") +'\n\nЗнаменатель\n\n1. ' + a[0][1] + '\n2. ' + a[0][2] + '\n3. ' + a[0][3][1] + '\n4. ' + a[0][4]
-    bot.send_message('-242014252', main_message)
+    bot.send_message('-1001322716320', main_message)
 
-schedule.every().day.at("0:05").do(main_message)
+schedule.every().day.at("00:40").do(main_message)
 while True:
     schedule.run_pending()
 bot.polling(none_stop=True, interval = 0)
