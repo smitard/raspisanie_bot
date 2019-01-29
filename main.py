7af2bf65b8e423fdd2681d32a2ec882386b47d72
smitard <45298247+smitard@users.noreply.github.com>
2019-01-29 23:07:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main_message():
         main_message =  a[0][0] + '  ' + tomorrowdate.strftime("%d.%m.%Y") +'\n\nЗнаменатель\n\n1. ' + a[0][1] + '\n2. ' + a[0][2] + '\n3. ' + a[0][3][1] + '\n4. ' + a[0][4]
     bot.send_message('-1001322716320', main_message)
 
-schedule.every().day.at("17:30").do(main_message)
+schedule.every().day.at("23:10").do(main_message)
 while True:
     schedule.run_pending()
     time.sleep(1)
